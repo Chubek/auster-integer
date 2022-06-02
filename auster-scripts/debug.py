b8c220f3581d85ac5a3aefbc39a866cca8ed889a
Chubak <chubakbidpaa@gmail.com>
2022-06-02 12:10:07 +0430

    new branch

--- auster-scripts/debug.py
+++ auster-scripts/debug.py
@@ -2,55 +2,95 @@ from asyncio.subprocess import PIPE
 import glob
 from multiprocessing import Event
 import os
+from re import T
 import sys
 import subprocess
 import threading
 import asyncio
 from queue import Queue
+import time
 
 class Stdout(threading.Thread):
     def __init__(self, p: subprocess.Popen[bytes]):
-        super.__init__()
-
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.kill_event = Event()
         self.p = p
 
     def run(self) -> None:
-        while True:
-            line = self.p.stdout.readline()
+        while not self.p.poll():
+            try:
+                lines = self.p.stdout.readlines()
 
-            print(line)
+                print('\n'.join([l.decode() for l in lines]))
+            except:
+                print("Process is terminated, no stdout")
+                break
+
+            if self.kill_event.is_set():
+                break
+
+    def kill(self):
+        self.kill_event.set()
+        
 
 class Stderr(threading.Thread):
     def __init__(self, p: subprocess.Popen[bytes]):
-        super.__init__()
-
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.kill_event = Event()
         self.p = p
 
-    def run(self) -> None:
-        while True:
-            line = self.p.stderr.readline()
+        
 
-            print(line)
+    def run(self) -> None:
+       while not self.p.poll():
+            try:
+                lines = self.p.stderr.readlines()
+
+                print('\n'.join([l.decode() for l in lines]))
+            except:
+                print("Process is terminated, no stderr")
+                break
+
+            if self.kill_event.is_set():
+                break
+
+
+    def kill(self):
+        self.kill_event.set()
+
 
 
 class Stdin(threading.Thread):
     def __init__(self, p: subprocess.Popen[bytes]):
-        super.__init__()
+        threading.Thread.__init__(self)
+        self.daemon = True
         self.message = ""
         self.event = threading.Event()
+        self.kill_event = Event()
         self.p = p
 
     def run(self) -> None:
-        while True:
-            if self.event.is_set():
-                print(f"Stdin got: {line}")
-                self.event.clear()
-                line = self.p.stdin.write(self.message)
+        self.event.wait()
+
+        res = self.p.communicate(self.message.encode())
+
+        self.event.clear()
+
+        if self.kill_event.is_set():
+            self.exit()
+        
     
 
     def __call__(self, *args, **kwg):
-        self.message = kwg.get("sep").join(args)
+        sep = kwg.get("sep") if kwg.get("sep") is not None else " "
+        self.message = sep.join(args)
         self.event.set()
+
+
+    def kill(self):
+        self.kill_event.set()
 
                 
 
@@ -97,40 +137,104 @@ if __name__ == "__main__":
             file_path = os.path.join(path, latest)
 
             subp = subprocess.Popen(
-                f"rust-lldb {file_path}".split(" "), 
+                f"rust-lldb {file_path}", 
                 stdout=subprocess.PIPE, 
                 stderr=subprocess.PIPE,
                 stdin=subprocess.PIPE,
                 shell=True,
             )
-            
+
             event = Event()
-            queue = Queue()
+            queue = Queue()            
+
+            stdin = Stdin(subp) 
+            stdin.start()
             
-            stdin = Stdin()
-            stdout = Stdout()
-            sys.stderr = Stderr()
-
-            def get_and_parse_input():
+            stdout = Stdout(subp)
+            stdout.start()
+            
+            stderr = Stderr(subp)
+            stderr.start()
+            
+            async def get_and_parse_input():
                 std_in = sys.stdin.readline()
-                queue.put(std_in)
+                queue.put(std_in.strip())
                 event.set()
+                
+                br = breakout()
+                if br:
+                    queue.put("break")
 
-            async def main_loop():
-                while True:
-                    if event.is_set:
-                        event.clear()
+                return True
+
+            async def quit():
+                global stdin, stdout, stderr
+
+                print("Exiting LLDB...")  
+
+
+                stdin.kill()
+                stdout.kill()
+                stderr.kill()
+
+                subp.communicate(b'quit')
+
+                subp.kill()
+                
+                print("Exited process with SIGKILL")
+
+                return True
+
+
+            def breakout():
+                poll = subp.poll()
+
+                if poll is not None:
+                    stdin.kill()
+                    stdout.kill()
+                    stderr.kill()
+                    print(f"Process exited with status {poll}")
+                    return True
+
+                else:
+                    return False
+
+            async def main_task():
+                while True:                 
+                    await get_and_parse_input()
+
+                    if event.is_set():
+                        event.clear()                    
+
                         popped = queue.get()
+                
 
                         match popped:
                             case 'exit': 
-                                return
+                                await quit()
+                                return True 
+                            case 'break':
+                                return True
                             case _:
                                 stdin(popped)
 
+                    
+                
 
-            loop = asyncio.get_event_loop()
 
-            loop.run_until_complete(main_loop)
+            async def main_loop():
+                task = asyncio.create_task(main_task())
+                done, _ = await asyncio.wait({ task })
 
+                if task in done:
+                    print("Task is done")
+                    return True
+        
+            
+            asyncio.run(main_loop())
+
+            print("Exiting script...")
+
+
+            sys.exit()
 
